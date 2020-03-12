5825677c430842ac37beb36827713496a72420dd
Bandi Praveen Kumar <61545125+bpraveeninfo@users.noreply.github.com>
2020-03-12 17:11:44 +0530

    add profilepic

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import ReactTypingEffect from 'react-typing-effect';
-import profilepic from '../img/profile.png';
-import Social from '../components/Social'
+import profilepic from '../img/profile_small.png';
+import bannerimage from '../img/comakeit.jpg';
+import Social from '../components/Social';
+import ResizeImage from 'react-resize-image';
 
 
 
@@ -9,10 +11,41 @@ class Home extends Component {
     render() {
         return (
             <div className="condiv home">
-            <img src={profilepic} className="profilepic"></img>
-            <ReactTypingEffect className="typingeffect" text={['I am Praveen Kumar Bandi','I am a UI and web developer']} speed={100} eraseDelay={700}/>
-            <Social></Social>
-            </div>
+            {/* <img src={profilepic} className="profilepic"></img>
+            <ReactTypingEffect className="typingeffect" text={['I am Praveen Kumar Bandi','I am a UI and web developer']} speed={100} eraseDelay={700}></ReactTypingEffect>
+            <Social/> */}
+
+
+
+            <div className="card-group">
+                <div className="card">
+                    <img src={bannerimage} class="card-img-top" alt="..."/>
+                    <div className="card-body author-area">
+                    <ResizeImage
+                   
+                        src={profilepic} 
+                        alt="Praveen Kumar Bandi"
+                        
+                        options={{ w: 200}}
+                    />
+
+                    {/* <img src={profilepic} class="card-img-top" alt="..." options={{ width:20 }}/> */}
+                    <h3 className="card-title">Praveen Kumar Bandi</h3>
+                    <h5>UI and Web <u>Developer</u> | UI/UX <u>Designer</u></h5>
+                    <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+
+
+
+
+</div>
+
+
+</div>
+
+            
+         
             )
         }
     }
