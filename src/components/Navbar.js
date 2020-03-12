8bbbd0b28e281c48b29f60d95ed7acedae7df3ed
praveendrpt <bpraveendrpt@gmail.com>
2020-03-13 00:04:24 +0530

    UPDATED URL

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ class Navbar extends Component {
         return (
             
             <nav className="navbar  navbar-light bg-light">
-                <a class="navbar-brand" href="#">BPK</a>
+                <a class="navbar-brand" href="http://bandipraveen.com/">BPK</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
